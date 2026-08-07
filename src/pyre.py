f6349beba1571863448c725b117c456464bed936
serenity-cell <justin1726hernandez2@gmail.com>
2026-08-07 03:31:33 +0000

    refactored main and implemented a dependency check for required tools

--- src/pyre.py
+++ src/pyre.py
@@ -1,25 +1,26 @@
-from rich.console import Console
 import argparse
 import nmap
+import shutil
 import subprocess
+import os
+from rich import print as rprint
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        prog="PYRE",
-        description="PYRE's your recon engine, is an auotmated recon tool to facilitate the initial recone stage",
-    )
-    parser.add_argument("-s", "--scan", help="-s <ip address>", type=str)
-    parser.add_argument("-d", "-dns", help="-d <website domain (google.com, etc)>")
-    parser.add_argument("-H", "--host")
+def check_dependencies():
+    REQUIRED_TOOLS = ["ffuf", "whatweb", "nikto", "nmap"]
+    for tool in REQUIRED_TOOLS:
+        if shutil.which(tool) == None:
+            rprint(
+                f"[bold red]\\[!] MISSING REQUIRED TOOL: {tool}[/bold red]",
+            )
 
-    args = parser.parse_args()
+
+def run_nmap(target_ip):
     # initializing important decleration
     nm = nmap.PortScanner()
-    target_ip = args.scan
 
     # scans target's top 1000 ports
-    nm.scan(target_ip, "1-1000", arguments="-T5 -sC -sV")
+    nm.scan(target_ip, "1-1000", arguments="-T5 -sC -sV -oN pyre_output.txt")
 
     for host in nm.all_hosts():
         print(f"Target: {host}")
@@ -33,6 +34,34 @@ def main():
                 print(
                     f"{port}\t{port_info['state']}\t{port_info['name']}\t{port_info['product']}\t{port_info['version']}\n"
                 )
+
+
+def run_ffuf(target_ip):
+
+    subprocess.Popen(
+        [
+            "ffuf",
+            "-u",
+            f"http://{target_ip}",
+            "-w",
+            "/usr/share/wordlists/seclists/Discovery/Web-Content/DirBuster-2007_directory-list-2.3-small.txt",
+        ]
+    )
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="PYRE",
+        description="PYRE's your recon engine, is an auotmated recon tool to facilitate the initial recone stage",
+    )
+    parser.add_argument("-s", "--scan", help="-s <ip address>", type=str)
+    parser.add_argument("-w", "--wordlist", help="-w path/to/wordlist.txt")
+    parser.add_argument("-H", "--host")
+    args = parser.parse_args()
+
+    check_dependencies()
+    run_nmap(args.scan)
+    run_ffuf(args.scan)
 
 
 main()
