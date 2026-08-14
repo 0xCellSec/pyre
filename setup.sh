cda673f1e2034997e8aad3cbce46515da6daf8a8
serenity-cell <justin1726hernandez2@gmail.com>
2026-08-14 04:10:01 +0000

    rework entire setup.sh to build .venv + create a pyre launcher

--- setup.sh
+++ setup.sh
@@ -1,9 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-file="requirements.txt"
+set -e
 
-echo "Downloading python dependencies...."
-for item in $(cat "$file"); do
-  echo "[+] Installing $item"
-  pip install $item >/dev/null
-done
+# stores the aboslute path where this bash script is at
+PYRE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+FILE="requirements.txt"
+
+echo "[+] Creating virtual enviornment..."
+python3 -m venv "$PYRE_DIR/.venv"
+
+echo "[+] Installing python dependencies..."
+"$PYRE_DIR/.venv/bin/python" -m pip install --upgrade pip
+"$PYRE_DIR/.venv/bin/python" -m pip install -r "$PYRE_DIR/requirements.txt"
+
+echo "[+] Creating Pyre Launched"
+mkdir -p "$HOME/.local/bin"
+
+cat >"$HOME/.local/bin/pyre" <<EOF
+#!/bin/bash
+exec "$PYRE_DIR/.venv/bin/python" "$PYRE_DIR/pyre.py" "\$@"
+EOF
+
+chmod +x "$HOME/.local/bin/pyre"
+
+echo "[+] PYRE has been installed!"
+echo "[+] To get started run: pyre --help"
