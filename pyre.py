17b63398a4b9b7603b4728744a8c980eb956959c
serenity-cell <justin1726hernandez2@gmail.com>
2026-08-16 08:23:15 +0000

    rename everything to be more self-documenting

--- pyre.py
+++ pyre.py
@@ -11,10 +11,10 @@ class Tools:
         with open("config.yaml", "r", encoding="utf-8") as file:
             self.data = yaml.safe_load(file)
 
-    def run_nmap(self, target_ip):
+    def run_nmap_and_fetch_port_list(self, target_ip):
         # initializing important decleration
         nm = nmap.PortScanner()
-        post_scan_list = []
+        list_ports = []
 
         # scans target's top 1000 ports
         nm.scan(target_ip, "1-1000", arguments="-T5 -sC -sV -oN pyre_output.txt")
@@ -24,16 +24,16 @@ class Tools:
             print(f"State: {nm[host].state()}")
             print("---------------------------------")
             for protocol in nm[host].all_protocols():
-                port_list = nm[host][protocol].keys()
+                protocol_port_list = nm[host][protocol].keys()
 
-                for port in sorted(port_list):
+                for port in sorted(protocol_port_list):
                     port_info = nm[host][protocol][port]
                     print(
                         f"{port}\t{port_info['state']}\t{port_info['name']}\t{port_info['product']}\t{port_info['version']}\n"
                     )
-                    post_scan_list.append(str(port))
+                    list_ports.append(str(port))
 
-        return post_scan_list
+        return list_ports
 
     def run_whatweb(self):
         subprocess.Popen(["whatweb", self.data["webpage"]])
@@ -71,27 +71,27 @@ class Tools:
         )
 
 
-def check_dependencies():
+def has_all_dependencies():
     # update this everytime a new tool is added
-    REQUIRED_TOOLS = ["ffuf", "whatweb", "nmap"]
-    missing_tool = False
-    for tool in REQUIRED_TOOLS:
+    LIST_REQUIRED_TOOLS = ["ffuf", "whatweb", "nmap"]
+    is_tool_missing = False
+    for tool in LIST_REQUIRED_TOOLS:
         if shutil.which(tool) == None:
             rprint(
                 f"[bold red]\\[!] MISSING REQUIRED TOOL: {tool}[/bold red]",
             )
-            missing_tool = True
-    return missing_tool
+            is_tool_missing = True
+    return is_tool_missing
 
 
-def handle_web_recon():
+def call_web_recon_tools():
     tools = Tools()
     # tools.run_whatweb()
     tools.run_directory_ffuf()
     tools.run_subdirectory_ffuf()
 
 
-DISPATCH = {"80": handle_web_recon}
+FETCH_TOOL_COMMAND = {"80": call_web_recon_tools}
 
 
 def main():
@@ -103,16 +103,16 @@ def main():
     parser.add_argument("-w", "--web", help="only does web recon")
     args = parser.parse_args()
 
-    if check_dependencies() == True:
+    if has_all_dependencies() == True:
         return
 
     tools = Tools()
 
-    nmap_results = tools.run_nmap(args.scan)
-    if nmap_results:
-        for port in nmap_results:
-            if str(port) in DISPATCH:
-                DISPATCH[str(port)]()
+    has_nmap_results = tools.run_nmap_and_fetch_port_list(args.scan)
+    if has_nmap_results:
+        for port in has_nmap_results:
+            if str(port) in FETCH_TOOL_COMMAND:
+                FETCH_TOOL_COMMAND[str(port)]()
     else:
         rprint("[bold red]\\[!] Ports scan is empty[bold red]")
         rprint("[bold blue]\\[+] Opening scan result for manual check")
